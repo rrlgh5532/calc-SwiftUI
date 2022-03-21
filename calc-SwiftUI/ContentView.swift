4e071525a3a895d8752a7271f0bb7b9e809bf98a
One27 <rrlgh5532@me.com>
2022-03-21 21:25:40 +0900

    Initial Commit

---
+++ calc-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  calc-SwiftUI
+//
+//  Created by 이기호 on 2022/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ calc-SwiftUI/calc_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  calc_SwiftUIApp.swift
+//  calc-SwiftUI
+//
+//  Created by 이기호 on 2022/03/21.
+//
+
+import SwiftUI
+
+@main
+struct calc_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
