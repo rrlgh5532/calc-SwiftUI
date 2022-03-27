c3c967c6fe4225f0696f2319318d81c57a312775
One27 <rrlgh5532@me.com>
2022-03-28 00:08:17 +0900

    test

--- calc-SwiftUI/ContentView.swift
+++ calc-SwiftUI/ContentView.swift
@@ -9,25 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            VStack{
-                Text("Hello, world!")
-                    .multilineTextAlignment(.center)
-                    .padding()
-                    .font(.largeTitle)
-                Text("Goodboy Wolrd").font(.custom("Copperplate", size: 70))
-                HStack{
-                    Text("Text3").padding().border(Color.black)
-                    Text("Text4")
-                }
-                MyHstackView()
-            }
-            Text("Text5")
-            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
-                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-            }.font(.largeTitle).foregroundColor(.black)
-                .background().colorMultiply(.white)
-        }
+        VStack{
+            HStack{
+                Text("test1")
+            }.fixedSize(horizontal: true, vertical: true)
+            HStack{
+                Text("Test2")
+            }.fixedSize(horizontal: true, vertical: false)
+            HStack{
+                Text("Test3")
+                
+            }.fixedSize(horizontal: true, vertical: false)
+        }.fixedSize(horizontal: true, vertical: true)
     }
 }
 
@@ -43,11 +36,10 @@ struct MyHstackView: View{
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            .previewDevice(PreviewDevice(rawValue: "iPhone SE"))
-            .previewDisplayName("iPhone SE")
-        ContentView()
             .previewDevice(PreviewDevice(rawValue: "iPhone 12 Pro"))
             .previewDisplayName("iPhone 12 Pro")
             .environment(\.colorScheme,.light)
+            .previewInterfaceOrientation(.landscapeRight)
+        ContentView().previewDevice("iPad mini (6th generation)").previewDisplayName("iPad mini").previewInterfaceOrientation(.landscapeLeft)
     }
 }
